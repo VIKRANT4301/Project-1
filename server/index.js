77b356f36a5ca9e9900a9aea8731b14ac50fd2c5
VIKRANT4301 <vikrantchakole36@gmail.com>
2025-01-23 23:19:52 +0530

    done

--- server/index.js
+++ server/index.js
@@ -19,7 +19,6 @@ if (!PORT) {
 
 // CORS configuration
 const allowedOrigins = [
-  process.env.FRONTEND_URL_LOCAL || "http://localhost:5174",  // Local environment
   process.env.REACT_APP_API_URL || "https://project-1-sage-phi.vercel.app/",  // Production frontend URL
 ];
 
